46563d418ecd973eb08f2d5eb04ecac1cec6a443
Marko Stamcar <marko@stamcar.com>
2020-05-13 18:48:50 +0200

    Fix for mobile browsers: soft keyboards do not send key events except for ENTER so we just take the last line from textarea without the prompt prefix

--- src/terminal.js
+++ src/terminal.js
@@ -128,7 +128,7 @@ const executor = commands => (cmd, ...args) => cb => {
 };
 
 // Handle keyboard events
-const keyboard = (parse) => {
+const keyboard = ($element, prompt, parse) => {
   let input = [];
   const keys = {8: 'backspace', 13: 'enter'};
   const ignoreKey = code => code >= 33 && code <= 40;
@@ -138,7 +138,7 @@ const keyboard = (parse) => {
     keypress: (ev) => {
       if (key(ev) === 'enter') {
         const str = input.join('').trim();
-        parse(str);
+        parse(str || $element.value.split(/\n/)[$element.value.split(/\n/).length - 1].replace(prompt(), ''));
         input = [];
       } else if (key(ev) !== 'backspace') {
         input.push(String.fromCharCode(ev.which || ev.keyCode));
@@ -190,7 +190,7 @@ export const terminal = (opts) => {
   const render = renderer(tickrate, onrender);
   const parse = parser(onparsed);
   const focus = () => setTimeout(() => $element.focus(), 1);
-  const kbd = keyboard(parse);
+  const kbd = keyboard($element, prompt, parse);
   const clear = () => ($element.value = '');
   const input = ev => busy
     ? ev.preventDefault()
